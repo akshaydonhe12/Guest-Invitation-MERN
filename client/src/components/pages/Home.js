b12bdfb55269a98d299a55d020adea18cc3bab74
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-06-26 12:33:10 +0530

    Guest-counter-table

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import GuestForm from '../guest/GuestForm';
+import GuestCounter from '../guest/GuestCounter';
+
 
 const Home = () => {
   return (
     <div className="app-container">
       <div className="main">
       <GuestForm />
+      <GuestCounter />
       </div>
     </div>
   )
