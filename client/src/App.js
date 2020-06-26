ea39951dab1e111b526717997d68e83ed95dfe2d
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-06-26 12:24:19 +0530

    guest-form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/layouts/Navbar'
+import Navbar from './components/layouts/Navbar';
+import Home from './components/pages/Home';
+
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GuestForm from '../guest/GuestForm';
+
+const Home = () => {
+  return (
+    <div className="app-container">
+      <GuestForm />
+    </div>
+  )
+}
+
+export default Home;
