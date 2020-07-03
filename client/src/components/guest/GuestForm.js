b53058cc1b7b4ec2be3c8b1a46b5c114cc52c304
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-07-03 11:58:47 +0530

    add-guest/console

--- client/src/components/guest/GuestForm.js
+++ client/src/components/guest/GuestForm.js
@@ -1,30 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 const GuestForm = () => {
-  return (
-    <div className="invite-section">
+  const [guest, setGuest] = useState({
+    name:'',
+    phone:'',
+    dietary:'Non-Veg'
+  })
+
+const {name, phone, dietary} = guest
+
+const handelChange = e => {
+  setGuest({
+    ...guest,
+    [e.target.name]:e.target.value
+  })
+}
+
+const onsubmit = e => {
+  e.preventDefault()
+ {/* console.log(guest) */}
+  setGuest({
+    name:'',
+    phone:'',
+    dietary:'Non-Veg'
+  })
+}
+
+return (
+   <div className="invite-section">
       <h1>Invite Here</h1>
-      <form >
-        <input type="text" placeholder="Name" name="name" />
-        <input type="text" placeholder="Phone" name="phone" />
-        <p className="options-label">Dietary</p>
-        <div className="options">
-          <label className="container">Non-veg
-        <input type="radio" name="dietary" Value='Non-Veg' />
-            <span className="checkmark"></span>
-          </label>
-          <label className="container">Vegan
-        <input type="radio" name="dietary" Value='Vegan' />
-            <span className="checkmark"></span>
-          </label>
-          <label className="container">Pescetarians
-        <input type="radio" name="dietary" Value='Pescetarians' />
-            <span className="checkmark"></span>
-          </label>
-        </div>
-        <input type="submit" value="Add Guest" className="btn" />
-      </form>
+      <form onSubmit={onsubmit}>
+          <input type="text" placeholder="Name" name="name" value={name} onChange={handelChange} />
+          <input type="text" placeholder="Phone" name="phone" value={phone} onChange={handelChange} />
+          <p className="options-label">Dietary</p>
+
+    <div className="options">
+      <label className="container">Non-veg
+        <input type="radio" name="dietary" Value='Non-Veg' checked={dietary=== 'Non-Veg'} onChange={handelChange} />
+        <span className="checkmark"></span>
+      </label>
+
+      <label className="container">Vegan
+        <input type="radio" name="dietary" Value='Vegan' onChange={handelChange} />
+        <span className="checkmark"></span>
+      </label>
+
+      <label className="container">Pescetarians
+        <input type="radio" name="dietary" Value='Pescetarians' onChange={handelChange} />
+        <span className="checkmark"></span>
+      </label>
     </div>
+
+        <input type="submit" value="Add Guest" className="btn" />
+    </form>
+  </div>
   )
 }
 
