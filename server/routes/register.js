c7189e36463aa68c309ab4cca20c8de7e3ffaa5f
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-07-06 12:27:07 +0530

    fix-bug/register-route

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,5 +1,6 @@
 const router  = require("express").Router();
 const {check, validationResult} = require('express-validator');
+const bcrypt = require('bcrypt')
 
 
 //User Model
@@ -11,8 +12,7 @@ router.post('/',
     check('email', 'Please provide a email').isEmail(),
     check('password', 'Pleasse provide 4 character password').isLength({min: 4 })
 ],
-
-(req, res)=> {
+async(req, res)=> {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         return res.status(400).json({
@@ -21,6 +21,25 @@ router.post('/',
     }
 
     const {name, email, password} = req.body
+    try{
+        let user = await User.findOne({email})
+        if(user){
+            return res.status(400).json({
+                msg:'user already exists'
+            })
+        }
+        user = new User({
+            name,
+            email,
+            password
+        })
+        //const salt = await bcrypt.genSalt(10)
+        //user.password = await bcrypt.hash(password, salt)
+
+        //await user.save()
+    }catch (err){
+
+    }
 })
 
 module.exports = router;
