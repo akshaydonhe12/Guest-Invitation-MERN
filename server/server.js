9fd24f9110b0f5974b1616157760735af02d66b9
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-07-06 11:35:25 +0530

    connect-DB

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express()
+
+const connectDB = require('./config/db');
+//connect to DB
+connectDB()
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,() => console.log(`server started at port ${PORT} `))
