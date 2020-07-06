bb0a68b9cae4bbce3557ff955f54a7930bb52e47
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-07-06 12:01:42 +0530

    check/validation-on-register-route

--- server/routes/register.js
+++ server/routes/register.js
@@ -4,9 +4,20 @@ const {check, validationResult} = require('express-validator');
 
 router.post('/',
 [
-    check ('name', 'Please provide a name').not().isEmpty()
+    check ('name', 'Please provide a name').not().isEmpty(),
+    check('email', 'Please provide a email').isEmail(),
+    check('password', 'Pleasse provide 4 character password').isLength({min: 4 })
 ],
+
 (req, res)=> {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            error: errors.array()
+        })
+    }
+    res.send('User Register Succesfully')
+
     const {name, email, password} = req.body
 })
 
