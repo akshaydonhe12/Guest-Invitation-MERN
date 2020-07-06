4c19f2502288a34ea8ad3b72b3fef651b73a7c60
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-07-06 12:51:51 +0530

    genrete-token

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,6 +1,7 @@
 const router  = require("express").Router();
 const {check, validationResult} = require('express-validator');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 //User Model
@@ -33,12 +34,26 @@ async(req, res)=> {
             email,
             password
         })
-        //const salt = await bcrypt.genSalt(10)
-        //user.password = await bcrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
 
-        //await user.save()
+        await user.save()
+
+        const payload = {
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(payload, process.env.SECRET, {
+            expiresIn:3600
+        }, (err, token)=> {
+            if(err) throw err
+            res.send({token})
+        })
+
     }catch (err){
-
+        console.error(err.message)
+        res.status(500).send('Server Error')
     }
 })
 
